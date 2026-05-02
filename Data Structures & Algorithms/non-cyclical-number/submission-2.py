36f9efaedc033204f560cd59ca24dc6493608625
Tom Cheah <tomcheah@berkeley.edu>
2026-05-01 20:44:14 -0400

    Remove isHappy function implementation
    Removed the implementation of the isHappy function, which included logic for checking happy numbers.

--- Data Structures & Algorithms/non-cyclical-number/submission-2.py
+++ Data Structures & Algorithms/non-cyclical-number/submission-2.py
@@ -1,25 +1,4 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        string_num = str(n)
-        digits = [int(digit) for digit in string_num]
-        seen_sums = set()
-
-        res = 0 
-        count = 0
-        while res != 1:
-            squares = [digit**2 for digit in digits]
-            res = sum(squares)
-            seen_sums.add(res)
-            string_num = str(res)
-            print(string_num)
-            digits = [int(digit) for digit in string_num]
-            print(f'{count=}')
-            count += 1
-            print(seen_sums)
-            if count >= 35:
-                return False
-
-        print(f'{res=}')
-        return res == 1
 
         
