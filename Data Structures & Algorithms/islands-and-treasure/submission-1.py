020e5054a60c394e29de6155ecf6a134089bdecd
Tom Cheah <tomcheah@berkeley.edu>
2026-05-21 19:53:06 -0400

    Add: islands-and-treasure - submission-1

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-1.py
@@ -0,0 +1,50 @@
+class Solution:
+    water = -1
+    treasure = 0
+    land = (2**31) - 1
+
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        '''
+        Multi-source BFS
+        '''
+
+        queue = deque() # (r, c)
+        num_rows = len(grid)
+        num_cols = len(grid[0])
+
+        def in_bounds(value, bound) -> bool:
+            return 0 <= value < bound
+
+        treasures = []
+
+        for r in range(num_rows):
+            for c in range(num_cols):
+                if grid[r][c] == self.treasure:
+                    treasures.append((r, c))
+
+        for treasure in treasures:
+            queue.append(treasure)
+
+        distance = 1
+        while queue:
+            for _ in range(len(queue)):
+                r, c = queue.popleft()
+
+                if in_bounds(r+1, num_rows) and grid[r+1][c] == self.land:
+                    grid[r+1][c] = distance
+                    queue.append((r+1, c))
+                
+                if in_bounds(r-1, num_rows) and grid[r-1][c] == self.land:
+                    grid[r-1][c] = distance
+                    queue.append((r-1, c))
+
+                if in_bounds(c+1, num_cols) and grid[r][c+1] == self.land:
+                    grid[r][c+1] = distance
+                    queue.append((r, c+1))
+            
+                if in_bounds(c-1, num_cols) and grid[r][c-1] == self.land:
+                    grid[r][c-1] = distance
+                    queue.append((r, c-1))
+
+            distance += 1
+
