ca5eb831e023233a9d320070554d41ad59bd73ea
Tom Cheah <tomcheah@berkeley.edu>
2026-06-04 00:12:59 -0400

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.py
@@ -0,0 +1,26 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        cache = {}
+        def helper(i: int) -> int:
+            '''
+            Returns maximum amount of money from position i to the end
+            '''
+            if i >= len(nums):
+                return 0
+
+            if i in cache:
+                return cache[i]
+
+            # take house i 
+            take = nums[i] + helper(i+2)
+
+            # skip house i 
+            skip = helper(i+1)
+
+            cache[i] = max(skip, take)
+
+            return cache[i]
+
+        return helper(0)
+            
+            
