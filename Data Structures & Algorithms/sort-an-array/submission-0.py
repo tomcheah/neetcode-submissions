b1d2c606991f33f30d4a4a1e8031734cfb195386
Tom Cheah <tomcheah@berkeley.edu>
2026-06-13 13:40:49 -0400

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.py
@@ -0,0 +1,34 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        def merge_sort(nums: List[int]) -> List[int]:
+            if len(nums) <= 1:
+                return nums
+
+            mid = len(nums) // 2
+
+            left = merge_sort(nums[:mid])
+            right = merge_sort(nums[mid:])
+
+            return merge(left, right)
+
+        def merge(left: List[int], right: List[int]) -> List[int]:
+            res = []
+            l, r = 0, 0
+
+            while l < len(left) and r < len(right):
+                if left[l] <= right[r]:
+                    res.append(left[l])
+                    l += 1
+                else:
+                    res.append(right[r])
+                    r += 1
+
+            if l < len(left):
+                res.extend(left[l:])
+            else:
+                res.extend(right[r:])
+
+            return res
+
+
+        return merge_sort(nums)
