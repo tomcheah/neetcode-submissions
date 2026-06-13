27abb812e4c02b81edaaac368266faf7adaaca78
Tom Cheah <tomcheah@berkeley.edu>
2026-06-13 15:42:07 -0400

    Add: ransom-note - submission-0

---
+++ Data Structures & Algorithms/ransom-note/submission-0.py
@@ -0,0 +1,12 @@
+from collections import Counter
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        ransom_counter = Counter(ransomNote)
+        magazine_counter = Counter(magazine)
+
+        for char, count in ransom_counter.items():
+            if magazine_counter[char] < count:
+                return False
+
+        return True
