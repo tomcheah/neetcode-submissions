697d16edfa80bd37ec819a92c15fe3a7dced988d
Tom Cheah <tomcheah@berkeley.edu>
2026-06-14 18:00:29 -0400

    Add: is-subsequence - submission-5

---
+++ Data Structures & Algorithms/is-subsequence/submission-5.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        s_pointer = 0
+        t_pointer = 0
+
+        if len(s) > len(t):
+            return False
+
+        while t_pointer < len(t) and s_pointer < len(s):
+            if t[t_pointer] == s[s_pointer]:
+                s_pointer += 1
+
+            t_pointer += 1
+
+        return s_pointer == len(s)
+
+
