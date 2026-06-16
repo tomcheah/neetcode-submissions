0258e763172cf0e934dab90e3047b5887489d66e
Tom Cheah <tomcheah@berkeley.edu>
2026-06-16 12:22:18 -0400

    Add: find-k-closest-elements - submission-3

---
+++ Data Structures & Algorithms/find-k-closest-elements/submission-3.py
@@ -0,0 +1,26 @@
+import heapq
+
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        '''
+        Since the array is sorted, the k closest elments will be contiguous in the input array
+        '''
+
+        l, r = 0, len(arr) - k
+
+        while l < r:
+            mid = (l + r) // 2
+            # distance between x and leftmost element of current window [mid:mid+k]
+            curr_window_distance = abs(arr[mid] - x)
+
+            # distance between x and leftmost element of next possible window [mid+k:]
+            next_window_distance = abs(arr[mid+k] - x)
+
+            if curr_window_distance > next_window_distance:
+                # search within next window
+                l = mid + 1
+            else:
+                # search within current window
+                r = mid
+
+        return arr[l:l+k]
