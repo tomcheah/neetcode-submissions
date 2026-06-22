09dfc0b5b5457255c14eb497d1cf59e89a8b7a32
Tom Cheah <tomcheah@berkeley.edu>
2026-06-22 14:50:52 -0400

    Add: task-scheduling - submission-7

---
+++ Data Structures & Algorithms/task-scheduling/submission-7.py
@@ -0,0 +1,42 @@
+import heapq
+from collections import defaultdict
+
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        '''
+        Greedy: Process the task with the highest frequency
+
+        State:
+            - max heap (count, task)
+            - min heap (eligible_time, task)
+
+        '''
+        available = [] # max heap (count, task)
+        cooldown = [] # min heap (eligible_time, count, task)
+        task_counts = defaultdict(int)
+
+        for task in tasks:
+            task_counts[task] += 1
+
+        for task, count in task_counts.items():
+            heap_item = (count, task)
+            heapq.heappush_max(available, heap_item)
+
+        t = 0
+        while available or cooldown:
+            # make tasks eligible if current time has passed the next eligible time
+            while cooldown and t >= cooldown[0][0]:
+                _, count, task = heapq.heappop(cooldown)
+                max_heap_item = (count, task)
+                heapq.heappush_max(available, max_heap_item)
+            
+            # task is available -> do the most frequent task; else idle
+            if available:
+                count, task = heapq.heappop_max(available)
+                if count - 1 > 0:
+                    min_heap_item = (t+n+1, count -1, task)
+                    heapq.heappush(cooldown, min_heap_item)
+
+            t += 1
+        
+        return t
