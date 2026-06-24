f8fec1312aac816598182b6744661cc2395e01a1
Tom Cheah <tomcheah@berkeley.edu>
2026-06-24 11:36:51 -0400

    Add: meeting-schedule-ii - submission-4

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-4.py
@@ -0,0 +1,28 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+from collections import defaultdict
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        changes = defaultdict(int)
+        for interval in intervals:
+            changes[interval.start] += 1
+            changes[interval.end] -= 1
+
+        meeting_rooms = 0
+        res = 0
+    
+        sorted_keys = sorted(changes.keys())
+
+        for x in sorted_keys:
+            meeting_rooms += changes[x]
+            res = max(meeting_rooms, res)
+        
+
+        return res
