281d62d2ac97117f242c9f8fffe7553df2aaee5d
Tom Cheah <tomcheah@berkeley.edu>
2026-07-06 15:47:54 -0400

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.py
@@ -0,0 +1,49 @@
+from collections import defaultdict
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # course -> prerequisite 
+        # preqsuite -> courses
+
+        courses = defaultdict(list)
+
+        for course, prerequisite in prerequisites: 
+            courses[course].append(prerequisite)
+
+        visiting = set()
+        visited = set() 
+
+        def dfs(course: int) -> bool:
+            '''
+            Returns whether this course can be completed or not
+            '''
+            if course in visiting:
+                return False
+
+            if course in visited:
+                return True 
+
+            if course not in courses: 
+                visited.add(course)
+                return True
+
+
+            visiting.add(course)
+
+            for prerequisite in courses[course]:
+                if not dfs(prerequisite):
+                    return False
+
+            visiting.remove(course)
+            visited.add(course)
+
+            return True
+
+
+
+        
+        for course in range(numCourses):
+            if not dfs(course):
+                return False
+
+        return True
